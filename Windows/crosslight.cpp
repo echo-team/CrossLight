b1bacad1505d8a41679406f024e2ec349ef8142e
hemmemm <bkbk.78@bk.ru>
2017-11-12 18:40:43 +0300

    Work continues
    1. Remade a color-changing method
    2. Added Global consts for colors
    this closes #1 and closes #3

--- Windows/crosslight.cpp
+++ Windows/crosslight.cpp
@@ -2,8 +2,31 @@
 #include <windows.h>
 
 
+#define BACKGROUND_WHITE BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED
+#define FOREGROUND_WHITE FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+
 using namespace std;
 
+
+enum ConsoleColor
+{
+    Black         = 0,
+    Blue          = 1,
+    Green         = 2,
+    Cyan          = 3,
+    Red           = 4,
+    Magenta       = 5,
+    Brown         = 6,
+    LightGray     = 7,
+    DarkGray      = 8,
+    LightBlue     = 9,
+    LightGreen    = 10,
+    LightCyan     = 11,
+    LightRed      = 12,
+    LightMagenta  = 13,
+    Yellow        = 14,
+    White         = 15
+};
 
 class Console
 {
@@ -58,16 +81,15 @@ class Console
         }
 
         /*
-            function that sets text attributes
-            all attributes described in main
+            function that changes text and background colors
 
             this function overwrites the previous value
             TO DO:
                 store previous value
         */
-        bool SetTextAttribute(WORD atributes)
+        bool SetColor(ConsoleColor text, ConsoleColor background)
         {
-            return SetConsoleTextAttribute(hStdout, atributes);
+            return SetConsoleTextAttribute(hStdout, (WORD)((background<<4)|text));
         }
 
 };
@@ -81,47 +103,11 @@ void COORDprint(COORD coords)
 
 int main()
 {
-    WORD attributesExample;
     COORD coords;
-    attributesExample = FOREGROUND_RED | BACKGROUND_BLUE;
-    /*
-                ALL POSSIBLES ATTRIBUTES
-                FOR SetTextAttribute:
-        FOREGROUND_BLUE 0x0001
-            Text color contains blue.
-        FOREGROUND_GREEN 0x0002
-            Text color contains green.
-        FOREGROUND_RED 0x0004
-            Text color contains red.
-        FOREGROUND_INTENSITY 0x0008
-            Text color is intensified.
-        BACKGROUND_BLUE 0x0010
-            Background color contains blue.
-        BACKGROUND_GREEN 0x0020
-            Background color contains green.
-        BACKGROUND_RED 0x0040
-            Background color contains red.
-        BACKGROUND_INTENSITY 0x0080
-            Background color is intensified.
-        COMMON_LVB_LEADING_BYTE 0x0100
-            Leading byte.
-        COMMON_LVB_TRAILING_BYTE 0x0200
-            Trailing byte.
-        COMMON_LVB_GRID_HORIZONTAL 0x0400
-            Top horizontal
-        COMMON_LVB_GRID_LVERTICAL 0x0800
-                Left vertical.
-        COMMON_LVB_GRID_RVERTICAL 0x1000
-            Right vertical.
-        COMMON_LVB_REVERSE_VIDEO 0x4000
-            Reverse foreground and background attribute.
-        COMMON_LVB_UNDERSCORE 0x8000
-            Underscore.
-    */
     Console mainconsole;
-    mainconsole.SetTextAttribute(attributesExample);
-    attributesExample = FOREGROUND_RED;
-    mainconsole.SetTextAttribute(attributesExample);
+    ConsoleColor exTextColor = White;
+    ConsoleColor exBackColor = Red;
+    mainconsole.SetColor(exTextColor, exBackColor);
     COORDprint(mainconsole.GetConsoleSize());
     return 0;
 }
